0ad3da54de5e5cb3f833596b98f08594d1edace7
skarner2016 <skarner2016@gmail.com>
2023-10-11 18:21:47 +0800

    test github webhook

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Skarner2016\Bark;
+
+/**
+ * @desc
+ * @author  skarner <2023-10-11 10:57>
+ */
+class Test
+{
+    public function index()
+    {
+        echo '(new Test())->index()' . PHP_EOL;
+    }
+}
+
+(new Test())->index();
+
